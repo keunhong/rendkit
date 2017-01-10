fee5d16a0f6b7e9af55f92e5aa5aca156f4f1013
kpar <kpar@cs.washington.edu>
2017-01-10 15:10:52 -0800

    Implement SVBRDF model to/from JSON serialization

--- svbrdf/models.py
+++ svbrdf/models.py
@@ -16,11 +16,23 @@ class SVBRDF(Base):
     substance_name = Column(String(100), nullable=True)
     path = Column(String(1024))
 
-    def get_jsd(self):
+    def to_jsd(self):
         return {
-            "type": 'svbrdf',
-            "path": self.path,
+            'type': 'svbrdf',
+            'path': self.path,
+            'id': self.name,
+            'name': self.name,
+            'substance_name': self.substance_name,
         }
+
+    @classmethod
+    def from_jsd(cls, jsd):
+        new = cls()
+        new.id = jsd['id']
+        new.path = jsd['path']
+        new.substance_name = jsd['substance_name']
+        new.name = jsd['name']
+        return new
 
 
 class SVBRDFRendering(Base):
