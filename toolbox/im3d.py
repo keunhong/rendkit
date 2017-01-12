cc60aa21bec6aed8c9d8150c5184757c812c4590
kpar <kpar@cs.washington.edu>
2017-01-11 18:25:23 -0800

    Use tangents when finding 3D extrema.

--- toolbox/im3d.py
+++ toolbox/im3d.py
@@ -2,7 +2,6 @@ import numpy as np
 from numpy import linalg
 from scipy.spatial import ConvexHull
 from skimage import transform
-from sklearn.decomposition import PCA
 from toolbox.data import reject_outliers
 
 
@@ -20,7 +19,7 @@ def _find_3d_extrema(hull_verts_3d, centroid, u, v):
                      centroid + max_u + min_v))
 
 
-def find_3d_bbox(coords_im, normals_im, region_mask) -> np.ndarray:
+def find_3d_bbox(coords_im, normals_im, tangents_im, region_mask) -> np.ndarray:
     """
     Finds the 3D bounding box of a planar region given a mask of a planar
     region. The bounding box is given as a 4-tuple which defines each corner
@@ -29,10 +28,8 @@ def find_3d_bbox(coords_im, normals_im, region_mask) -> np.ndarray:
     The algorithm is as follows:
         1. Find 2D convex hull of region.
         2. Unproject hull vertices onto 3D space.
-        3. Perform PCA on region coordinates to find dominant directions.
-        4. Adjust PCA directions (u, v) so they define an orthonormal basis
-           with the normal.
-        5. Find extrema in the u, v directions which defines the corners.
+        3. Use tangent/bitangent directions as u, v directions.
+        4. Find extrema in the u, v directions which defines the corners.
     """
     yx = np.vstack(np.where(region_mask)).T
     hull = ConvexHull(yx)
@@ -40,17 +37,18 @@ def find_3d_bbox(coords_im, normals_im, region_mask) -> np.ndarray:
     hull_verts_3d = coords_im[hull_verts_2d[:, 0], hull_verts_2d[:, 1], :]
 
     normals = reject_outliers(normals_im[region_mask], thres=1)
+    tangents = reject_outliers(tangents_im[region_mask], thres=1)
 
     n = np.mean(normals, axis=0)
     n /= linalg.norm(n)
+    u = np.mean(tangents, axis=0)
+    u /= linalg.norm(u)
 
-    pca = PCA(n_components=2)
-    pca.fit(coords_im[region_mask])
     # Make sure u, v, n form an orthonormal basis.
-    u = pca.components_[0, :]
     v = np.cross(u, n)
     u = np.cross(n, v)
-    return _find_3d_extrema(hull_verts_3d, pca.mean_, u, v)
+    centroid = np.mean(coords_im[region_mask], axis=0)
+    return _find_3d_extrema(hull_verts_3d, centroid, u, v)
 
 
 def rectify_plane(image, corners, corners_3d, scale=None):
