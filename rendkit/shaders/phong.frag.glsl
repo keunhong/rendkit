dbd578d347f1ed55eac5208cd501dcb0bd03f772
Keunhong Park <kpar@cs.washington.edu>
2017-02-23 03:18:34 -0800

    Implement importance sampling for Phong.

--- rendkit/shaders/phong.frag.glsl
+++ rendkit/shaders/phong.frag.glsl
@@ -1,10 +1,12 @@
-#version 120
+#version 150
+#include "utils/math.glsl"
+#include "utils/sampling.glsl"
 
 #define LIGHT_POINT 0
 #define LIGHT_DIRECTIONAL 1
 
-varying vec3 v_position;
-varying vec3 v_normal;
+in vec3 v_position;
+in vec3 v_normal;
 
 uniform vec3 u_cam_pos;
 uniform vec3 u_diff;
@@ -19,8 +21,8 @@ uniform int u_light_type[TPL.num_lights];
 #endif
 
 #if TPL.use_radiance_map
-uniform sampler2D u_radiance_map;
-uniform vec2 u_radiance_map_size;
+uniform samplerCube u_irradiance_map;
+uniform samplerCube u_radiance_map;
 #endif
 
 const float NUM_LIGHTS = TPL.num_lights;
@@ -29,6 +31,13 @@ vec3 irradiance(vec3 N, vec3 L, vec3 light_color) {
   float cosine_term = max(.0, dot(N, L));
   return cosine_term * light_color;
 }
+
+vec2 compute_sample_angles(vec2 xi) {
+  float phi = 2.0f * M_PI * xi.x;
+  float theta = acos(pow(1 - xi.y, 1.0/(u_shininess + 1)));
+  return vec2(phi, theta);
+}
+
 
 void main() {
   vec3 V = normalize(u_cam_pos - v_position);
@@ -42,44 +51,27 @@ void main() {
 	vec3 total_radiance = vec3(0.0);
 
   #if TPL.use_radiance_map
-  float radiance_map_area = u_radiance_map_size.x * u_radiance_map_size.y;
-  for (float s = 0; s <= 1.0; s += 1.0/u_radiance_map_size.s) {
-    for (float t = 0; t <= 1.0; t += 1.0/u_radiance_map_size.t) {
-      float x = s * 2 - 1;
-      float z = t * 2 - 1;
-      if (x*x + z*z <= 1) {
-        vec2 samp_ind = vec2((z+1)/2, (x+1)/2);
-        vec3 L = vec3(x, sqrt(max(0, 1 - x*x - z*z)), z);
-        vec3 light_color = texture2D(u_radiance_map, samp_ind).rgb;
-        vec3 irradiance = irradiance(v_normal, L, light_color);
-        vec3 radiance = u_diff * irradiance;
-        total_radiance += radiance / radiance_map_area;
-      }
-    }
+  total_radiance += u_diff * texture(u_irradiance_map, v_normal).rgb;
+  vec3 specular = vec3(0);
+  float r1 = rand(v_position.xy);
+  float r2 = rand(vec2(r1));
+  int N_SAMPLES = 128;
+  for (int i = 0; i < N_SAMPLES; i++) {
+    vec2 xi = vec2(rand(vec2(r1 + i, r2 + i)));
+    vec2 sample_angle = compute_sample_angles(xi);
+    float phi = sample_angle.x;
+    float theta = sample_angle.y;
+    vec3 H = sample_to_world(phi, theta, v_normal);
+    vec3 L = reflect(-V, H);
+    vec3 light_color = texture(u_radiance_map, L).rgb;
+    float ndotl = max(0.0, dot(v_normal, L));
+		vec3 refl_dir = normalize(2.0 * ndotl * v_normal - L);
+		float rdotv = max(0.0, dot(refl_dir, V));
+		vec3 Is = u_spec * pow(rdotv, u_shininess);
+    specular += irradiance(v_normal, L, light_color) * Is * ndotl;
   }
-
-  vec3 Q = reflect(-V, v_normal);
-  float samp_radius = 0.25;
-  float spec_area = radiance_map_area / (4.0 * samp_radius * samp_radius);
-  for (float s = 0; s <= 1.0; s += 1.0/u_radiance_map_size.s) {
-    for (float t = 0; t <= 1.0; t += 1.0/u_radiance_map_size.t) {
-      float x = Q.x + (2 * s - 1) * samp_radius;
-      float z = Q.z + (2 * t - 1) * samp_radius;
-      if (x*x + z*z <= 1) {
-        vec2 samp_ind = vec2((z + 1.0) / 2.0,
-                             (x + 1.0) / 2.0);
-        vec3 L = vec3(x, sqrt(max(0, 1 - x*x - z*z)), z);
-        vec3 H = normalize(L + V);
-        vec3 light_color = texture2D(u_radiance_map, samp_ind).rgb;
-        float ndoth = max(0.0, dot(v_normal, H));
-        vec3 Is = u_spec * pow(ndoth, u_shininess);
-        vec3 irradiance = irradiance(v_normal, L, light_color);
-        vec3 radiance = Is * irradiance;
-        total_radiance += radiance / spec_area;
-      }
-    }
-  }
-  // TODO: Lower hemisphere.
+  specular /= N_SAMPLES;
+  total_radiance += specular;
   #endif
 
 	#if TPL.num_lights > 0
